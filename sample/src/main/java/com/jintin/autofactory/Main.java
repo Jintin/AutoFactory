7f334d94b41032e7b3c4827c97ea90379ea2e28c
Jintin <jintin.lin1018@gmail.com>
2019-06-06 21:15:16 +0800

    update

--- sample/src/main/java/com/jintin/autofactory/Main.java
+++
@@ -1,9 +0,0 @@
-package com.jintin.autofactory;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Animal dog = AnimalFactory.createAnimal(AnimalTags.DOG);
-        dog.bark();
-    }
-}
